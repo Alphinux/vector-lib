6cd58560663a3f95ba5cfcc866b03d19557aa9c0
Tim Ruser <tim@ruser.dev>
2023-12-05 20:29:46 +0100

    Deleted Build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Tim Ruser/Documents/Dev/cpp/vector-lib-cpp
-# Build directory: C:/Users/Tim Ruser/Documents/Dev/cpp/vector-lib-cpp/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
